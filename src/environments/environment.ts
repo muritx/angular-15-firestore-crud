f8df7f697bb776d50dbbd61ed50ffcd07decb05d
Murilo Teixeira <73003137+muritx@users.noreply.github.com>
2023-05-27 17:42:57 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'xxx',
-    authDomain: 'bezkoder-firebase.firebaseapp.com',
-    databaseURL: 'https://bezkoder-firebase.firebaseio.com',
-    projectId: 'bezkoder-firebase',
-    storageBucket: 'bezkoder-firebase.appspot.com',
-    messagingSenderId: 'xxx',
-    appId: 'xxx'
+    apiKey: "AIzaSyBHQi2QWACVXbVTxzJQEGwcUoN_glCtxPE",
+    authDomain: "angular-crud-a59b8.firebaseapp.com",
+    projectId: "angular-crud-a59b8",
+    storageBucket: "angular-crud-a59b8.appspot.com",
+    messagingSenderId: "153698399488",
+    appId: "1:153698399488:web:978d2d3c8586be7cb3afe5",
+    measurementId: "G-2NRQWKEB04"
   }
 };
